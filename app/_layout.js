1f773301587556daa672fa7e571f4321d01f06b4
Carlos Eduardo Zampiere <eduardo@carteiraglobal.com>
2023-09-03 17:37:38 -0300

    init project

---
+++ app/_layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+import { Stack } from "expo-router";
+// import { Container } from './styles';
+
+const Layout = () => {
+  return <Stack />;
+};
+
+export default Layout;
